1cc0d0105266c315fad60c178b9eac51dc118673
Carlos Souza <carloshrsouza@gmail.com>
2024-01-21 23:50:41 -0500

    Add to db from chain sync client

--- lib/explorer/dashboard.ex
+++ lib/explorer/dashboard.ex
@@ -8,12 +8,56 @@ defmodule Explorer.Dashboard do
     Phoenix.PubSub.subscribe(@pub_sub, @new_blocks)
   end
 
-  def broadcast_new_block(block) do
-    Phoenix.PubSub.broadcast(@pub_sub, @new_blocks, {:new_block, block})
+  def broadcast_new_block(new_block, block_to_be_removed) do
+    Phoenix.PubSub.broadcast(@pub_sub, @new_blocks, {:new_block, new_block, block_to_be_removed})
   end
 
   @spec load_existing_blocks() :: list()
   def load_existing_blocks do
     BlocksDb.get_all_blocks()
+  end
+
+  @type new_block :: map()
+  @type block_to_be_removed :: map()
+
+  @max_blocks_per_page 10
+
+  @spec update_blocks_db(block_from_xogmios :: map()) :: {new_block, block_to_be_removed}
+  def update_blocks_db(block) do
+    ada_output =
+      block["transactions"]
+      |> Stream.flat_map(fn tx -> tx["outputs"] end)
+      |> Stream.map(fn output -> output["value"]["ada"]["lovelace"] end)
+      |> Enum.sum()
+      |> Decimal.div(1_000_000)
+      |> Decimal.round(4)
+      |> to_string()
+
+    fees =
+      Stream.map(block["transactions"], fn tx ->
+        tx["fee"]["lovelace"]
+      end)
+      |> Enum.sum()
+      |> Decimal.div(1_000_000)
+      |> Decimal.round(4)
+      |> to_string()
+
+    new_block = %{
+      block_id: block["id"],
+      block_size: block["size"]["bytes"],
+      block_height: block["height"],
+      issuer: block["issuer"],
+      tx_count: Enum.count(block["transactions"]),
+      ada_output: ada_output,
+      fees: fees
+    }
+
+    all_blocks = BlocksDb.get_all_blocks()
+    # First, get the oldest / @max_blocks_per_page-nth block on the db
+    block_to_be_removed = Enum.at(all_blocks, @max_blocks_per_page - 1)
+
+    BlocksDb.add_block(new_block)
+
+    {new_block, block_to_be_removed}
   end
 end
--- lib/explorer_web/live/dashboard_live.ex
+++ lib/explorer_web/live/dashboard_live.ex
@@ -1,7 +1,6 @@
 defmodule ExplorerWeb.DashboardLive do
   use ExplorerWeb, :live_view
 
-  alias Explorer.BlocksDb
   alias Explorer.Dashboard
 
   @impl true
@@ -24,42 +23,20 @@ defmodule ExplorerWeb.DashboardLive do
   end
 
   @impl true
-  def handle_info({:new_block, block}, socket) do
-    ada_output =
-      block["transactions"]
-      |> Stream.flat_map(fn tx -> tx["outputs"] end)
-      |> Stream.map(fn output -> output["value"]["ada"]["lovelace"] end)
-      |> Enum.sum()
-      |> Decimal.div(1_000_000)
-      |> Decimal.round(4)
-      |> to_string()
-
-    fees =
-      Stream.map(block["transactions"], fn tx ->
-        tx["fee"]["lovelace"]
-      end)
-      |> Enum.sum()
-      |> Decimal.div(1_000_000)
-      |> Decimal.round(4)
-      |> to_string()
-
-    new_block = %{
-      block_id: block["id"],
-      block_size: block["size"]["bytes"],
-      block_height: block["height"],
-      issuer: block["issuer"],
-      tx_count: Enum.count(block["transactions"]),
-      ada_output: ada_output,
-      fees: fees
-    }
-
-    BlocksDb.add_block(new_block)
-
+  def handle_info({:new_block, new_block, block_to_be_removed}, socket) do
     send(self(), :reset_counter)
 
-    IO.puts("new block")
+    new_socket =
+      socket
+      |> stream_insert(:blocks, new_block, at: 0)
+      |> then(fn socket ->
+        if block_to_be_removed do
+          stream_delete_by_dom_id(socket, :blocks, block_to_be_removed.block_id)
+        else
+          socket
+        end
+      end)
 
-    new_socket = stream_insert(socket, :blocks, new_block, at: 0)
     {:noreply, new_socket}
   end
 
@@ -72,15 +49,15 @@ defmodule ExplorerWeb.DashboardLive do
   def render(assigns) do
     ~H"""
     <div class="hidden md:block md:flex items-center justify-between py-3 text-sm">
-      <h2 class="inline-block text-2xl sm:text-3xl font-extrabold tracking-tight dark:text-slate-200 mb-10">
+      <h2 class="inline-block text-2xl sm:text-3xl font-extrabold tracking-tight text-slate-200 mb-10">
         Latest Blocks
       </h2>
 
       <div class="ml-auto flex flex-col">
-        <span class="inline-block sm:text-lg font-extrabold tracking-tight dark:text-slate-200 w-max">
+        <span class="inline-block sm:text-lg font-extrabold tracking-tight text-slate-200 w-max">
           Last updated
         </span>
-        <span phx-hook="timer" id="timer-1" class="last-updated sm:text-sml dark:text-slate-200">
+        <span phx-hook="timer" id="timer-1" class="last-updated sm:text-sml text-slate-200">
           0 seconds ago
         </span>
       </div>
@@ -90,22 +67,22 @@ defmodule ExplorerWeb.DashboardLive do
       <table class="border-collapse table-auto w-full text-sm">
         <thead>
           <tr>
-            <th class="border-b dark:border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
+            <th class="border-b border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
               Block (Height)
             </th>
-            <th class="border-b dark:border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
+            <th class="border-b border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
               ID
             </th>
-            <th class="border-b dark:border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
+            <th class="border-b border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
               Size
             </th>
-            <th class="border-b dark:border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
+            <th class="border-b border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
               Tx Count
             </th>
-            <th class="border-b dark:border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
+            <th class="border-b border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
               ADA Output
             </th>
-            <th class="border-b dark:border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
+            <th class="border-b border-slate-600 font-medium p-2 pt-0 pb-3 text-center">
               Fees
             </th>
           </tr>
@@ -114,24 +91,24 @@ defmodule ExplorerWeb.DashboardLive do
           <tr
             :for={{dom_id, block} <- assigns.streams.blocks}
             id={dom_id}
-            class="dark:bg-slate-800 hover:bg-slate-700 animate-fadeIn"
+            class="bg-slate-800 hover:bg-slate-700 animate-fadeIn"
           >
-            <td class="border-b border-slate-100 dark:border-slate-700 p-2 dark:text-slate-400 text-center">
+            <td class="border-b border-slate-100 border-slate-700 p-2 text-slate-400 text-center">
               <%= block.block_height %>
             </td>
-            <td class="border-b border-slate-100 dark:border-slate-700 p-2 dark:text-slate-400 text-center">
+            <td class="border-b border-slate-100 border-slate-700 p-2 text-slate-400 text-center">
               <%= String.slice(block.block_id, 0..7) %>
             </td>
-            <td class="border-b border-slate-100 dark:border-slate-700 p-2 dark:text-slate-400 text-center">
+            <td class="border-b border-slate-100 border-slate-700 p-2 text-slate-400 text-center">
               <%= block.block_size %>
             </td>
-            <td class="border-b border-slate-100 dark:border-slate-700 p-2 dark:text-slate-400 text-center">
+            <td class="border-b border-slate-100 border-slate-700 p-2 text-slate-400 text-center">
               <%= block.tx_count %>
             </td>
-            <td class="border-b border-slate-100 dark:border-slate-700 p-2 dark:text-slate-400 text-center">
+            <td class="border-b border-slate-100 border-slate-700 p-2 text-slate-400 text-center">
               <%= block.ada_output %>
             </td>
-            <td class="border-b border-slate-100 dark:border-slate-700 p-2 dark:text-slate-400 text-center">
+            <td class="border-b border-slate-100 border-slate-700 p-2 text-slate-400 text-center">
               <%= block.fees %>
             </td>
           </tr>
